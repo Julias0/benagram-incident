1c477f4a841645c9394c3d4ee672bf1d9a09d9ac
julias0 <aiyush.dhar@fyle.in>
2025-01-15 19:14:07 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'benagram_incident',
+  webDir: 'www'
+};
+
+export default config;
