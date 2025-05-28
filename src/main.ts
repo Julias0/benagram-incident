4064ab7013230f5e8ce0b32a5e362aaa97707829
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2025-05-28 19:17:02 +0530

    Added changes to change routing strategy to hash based

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -18,6 +19,7 @@ posthog.init(
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
